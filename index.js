6e0e718ba78b7a073ce13825738864db30da7f3f
vladimirKartyshov <vovan198080@inbox.ru>
2020-04-16 19:31:15 +0300

    Регистрация роутов

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const path = require('path')
 const exphbs = require('express-handlebars')
+const homeRoutes = require('./routes/home')
+const addRoutes = require('./routes/add')
+const coursesRoutes = require('./routes/courses')
 
 
 const app = express()
@@ -15,29 +18,9 @@ app.set('view engine', 'hbs')
 app.set('views', 'views')
 
 app.use(express.static('public'))
-
-
-
-app.get('/', (req, res) => {
-   res.render('index', {
-       title: 'Главная страница',
-       isHome: true
-   })
-})
-
-app.get('/add', (req, res) => {
-    res.render('add', {
-        title: 'Добавить новый курс',
-        isAdd: true
-    })
-})
-
-app.get('/courses', (req, res) => {
-    res.render('courses', {
-        title: 'Курсы',
-        isCourses: true
-    })
-})
+app.use('/', homeRoutes)
+app.use('/add', addRoutes)
+app.use('/courses', coursesRoutes)
 
 
 const PORT = process.env.PORT || 3000
---
+++ routes/add.js
@@ -0,0 +1,14 @@
+const {Router} = require('express')
+const router = Router()
+
+
+
+router.get('/', (req, res) => {
+    res.render('add', {
+        title: 'Добавить новый курс',
+        isAdd: true
+    })
+})
+
+
+module.exports = router
