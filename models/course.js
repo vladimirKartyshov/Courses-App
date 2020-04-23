421329bec34f99bc567a77d45bb0edb41042c595
vladimirKartyshov <vovan198080@inbox.ru>
2020-04-23 18:03:43 +0300

    Динамические параметры

--- models/course.js
+++ models/course.js
@@ -55,5 +55,10 @@ class Course {
         })
 
     }
+
+    static async getById(id){
+       const courses = await Course.getAll()
+        return courses.find(c => c.id === id)
+    }
 }
     module.exports = Course
