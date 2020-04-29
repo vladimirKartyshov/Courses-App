85c129cef153fd78b00823c67e6ea34325f21237
vladimirKartyshov <vovan198080@inbox.ru>
2020-04-29 14:28:59 +0300

    Добавили корзину

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 const exphbs = require('express-handlebars')
 const homeRoutes = require('./routes/home')
+const cardRoutes = require('./routes/card')
 const addRoutes = require('./routes/add')
 const coursesRoutes = require('./routes/courses')
 
@@ -19,9 +20,11 @@ app.set('views', 'views')
 
 app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
+
 app.use('/', homeRoutes)
 app.use('/add', addRoutes)
 app.use('/courses', coursesRoutes)
+app.use('/card', cardRoutes)
 
 
 const PORT = process.env.PORT || 3000
