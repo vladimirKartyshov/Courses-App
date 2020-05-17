91fb28fd6ca00f07ccecf0f69bf225c453916a59
vladimirKartyshov <vovan198080@inbox.ru>
2020-05-17 18:37:14 +0300

    Модель пользователя

---
+++ models/user.js
@@ -0,0 +1,33 @@
+const {Schema, model} = require('mongoose')
+
+const userSchema = new Schema({
+    email: {
+       type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+
+    cart: {
+        items: [
+            {
+                count: {
+                    type: Number,
+                    required: true,
+                    default: 1
+                },
+                courseId: {
+                    type: Schema.Types.ObjectID,
+                    ref: 'Course',
+                    required: true
+                }
+            }
+        ]
+    }
+})
+
+
+
+model.exports = model('User', userSchema)
